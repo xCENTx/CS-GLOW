9a13b62157a92a558e740d05c04e9f6884722f47
xCENTx <80198020+xCENTx@users.noreply.github.com>
2021-10-19 04:35:07 -0400

    updated offsets

--- helpers/offsets.cs
+++ helpers/offsets.cs
@@ -2,10 +2,10 @@
 {
     struct offsets
     {
-        public static int EntityList = 0x4DBD61C;
+        public static int EntityList = 0x4DBD5CC;
         public static int LocalPlayer = 0xDA344C;
         public static int TeamNum = 0xF4;
-        public static int GlowObjectManager = 0x5305B18;
+        public static int GlowObjectManager = 0x5305AE0;
         public static int GlowIndex = 0x10488;
         public static int Dormant = 0xED;
         public static int health = 0x100;
